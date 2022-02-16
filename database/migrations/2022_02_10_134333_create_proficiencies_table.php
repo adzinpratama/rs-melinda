b605259d40a088d9985b55c7c0d6c4f8195d4778
adzinpratama <hilmiadzinpratama@gmail.com>
2022-02-16 12:34:55 +0700

    fix migration to seeder

--- database/migrations/2022_02_10_134333_create_proficiencies_table.php
+++ database/migrations/2022_02_10_134333_create_proficiencies_table.php
@@ -16,6 +16,7 @@ class CreateProficienciesTable extends Migration
         Schema::create('proficiencies', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->timestamps();
         });
     }
 
